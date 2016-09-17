a95e55b9c78a8ec10085fdec0d1c06ef34f279a1
haojintang <haojintang@126.com>
2016-09-17 18:37:06 +0800

    autolayoutdemo03

---
+++ UIDemo/autolayoutAndstoryboard/AutoLayoutDemo03/AutoLayoutDemo03/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayoutDemo03
+//
+//  Created by haojin on 16/9/17.
+//  Copyright © 2016年 haojin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ UIDemo/autolayoutAndstoryboard/AutoLayoutDemo03/AutoLayoutDemo03/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutDemo03
+//
+//  Created by haojin on 16/9/17.
+//  Copyright © 2016年 haojin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
